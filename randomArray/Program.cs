21f3bbb1994640842a3d427c81fa7b61739a1405
Artur <arturnl@ya.ru>
2021-11-02 23:06:08 +0300

    Random Array

---
+++ randomArray/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] a = new int[10];
+Random rnd = new Random();
+for (int i = 0; i < 10; i++)
+{
+    a[i]=rnd.Next(1,100);
+}
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(a[i]+" ");
+}
+
+
